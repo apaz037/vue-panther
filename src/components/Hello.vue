f7bc85ab04d5a37b61a550a1f222d3f4cd3e5d8c
Aaron Paz <aaron@aarons-mbp.lan>
2017-09-01 10:29:26 -0400

    First component, template, router, eslint fixes, fixed dependencies, index.html

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1> {{ message }} </h1>
+  </div>
+</template>
+
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {};
+  },
+  computed: mapState({
+    message: state => state.Hello.message,
+  }),
+};
+</script>
